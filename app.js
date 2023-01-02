9ddc60243965486c4cfd9e28286abfc7e4ff7d5b
Nicolas Fusari <nick.fusari96@gmail.com>
2023-01-02 17:49:50 +0100

    végleseg Javascript

--- app.js
+++ app.js
@@ -3,4 +3,53 @@ let minus = document.getElementById('minus');
     reset = document.getElementById('reset');
     result = document.getElementById('result');
 
+
+plus.addEventListener("click", ()=>{
+
+    let value = parseInt(result.innerText);
+
+    let newValue = value +1;
+
+    result.innerText = newValue;
     
+    if(newValue > 0){
+
+        result.style.color = "green";
+    }
+    else if(newValue == 0){
+
+        result.style.color = "black";
+    }
+    else{
+
+        result.style.color = "red";
+    }
+})
+
+minus.addEventListener("click", ()=>{
+
+    let value = parseInt(result.innerText);
+
+    let newValue = value - 1;
+    
+    result.innerText = newValue;
+    
+    if(newValue > 0){
+
+        result.style.color = "green";
+    }
+    else if(newValue == 0){
+
+        result.style.color = "black";
+    }
+    else{
+
+        result.style.color = "red";
+    }
+})
+
+reset.addEventListener("click", ()=>{
+
+    result.innerText = 0;
+    result.style.color = "black";
+})
