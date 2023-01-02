4282d1be6f45a3dd028ec5c53b21986e26d5815b
Nicolas Fusari <nick.fusari96@gmail.com>
2023-01-02 17:31:20 +0100

    Alap javascript hozzáadva

---
+++ app.js
@@ -0,0 +1,6 @@
+let minus = document.getElementById('minus');
+    plus = document.getElementById('plus');
+    reset = document.getElementById('reset');
+    result = document.getElementById('result');
+
+    
